601759873ad3bab645eb5d94516544b4a50e0e8f
Калоян Димитров <luckykaloyan@gmail.com>
2023-12-21 10:53:21 +0200

    Update

--- EntryModule/FirstStepsInCoding/Lab/ConcatenateData.js
+++ EntryModule/FirstStepsInCoding/Lab/ConcatenateData.js
@@ -2,4 +2,3 @@ function ConcatenateData(dataArray){
     const [firstname, lastname, age, town] = dataArray;
     console.log(`You are ${firstname} ${lastname}, a ${age}-years old person from ${town}.`);
 }
- ConcatenateData(['Maria', 'Ivanova', 20, 'Sofia'])
