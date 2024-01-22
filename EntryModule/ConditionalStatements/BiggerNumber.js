2f96eeca088f2a6a9eba829be31bbe076978692c
Калоян Димитров <luckykaloyan@gmail.com>
2024-01-22 10:40:12 +0200

    Update

---
+++ EntryModule/ConditionalStatements/BiggerNumber.js
@@ -0,0 +1,14 @@
+function number(input){
+
+ let  a = parseInt(input[0]);
+ let b = parseInt(input[1]);
+
+ if(a>b){
+    console.log(a);
+ }else(console.log(b));
+
+
+
+}number((["5",
+
+"3"]))
--- EntryModule/ConditionalStatements/Excellent.js
+++ EntryModule/ConditionalStatements/Excellent.js
@@ -2,4 +2,4 @@ function excellent(input){
     if(input[0]=="6"){
         console.log("Excellent!");
     }
-}excellent("5")
+}
