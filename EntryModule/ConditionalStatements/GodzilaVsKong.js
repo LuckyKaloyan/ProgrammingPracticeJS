185b87791d601fd77531e6c0b44c8b03c7f45d69
Калоян Димитров <luckykaloyan@gmail.com>
2024-01-25 10:37:33 +0200

    Update

--- EntryModule/ConditionalStatements/GodzilaVsKong.js
+++ EntryModule/ConditionalStatements/GodzilaVsKong.js
@@ -8,12 +8,9 @@ if(statists>149){
     statists=statists*0.9;
 }
 priceClothes = priceClothes*statists;
-
 if(priceClothes+decor>budget){
     console.log("Not enough money!\nWingard needs "+((priceClothes+decor)-budget).toFixed(2)+" leva more.");
 }else{
     console.log("Action!\nWingard starts filimg with "+(budget-(priceClothes+decor)).toFixed(2)+" leva left.");
 }
-
-
-}GodzilaVsKong(["20000","120","55.5"])
+}
