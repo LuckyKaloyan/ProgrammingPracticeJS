beb166bf3a1c70e498ba932c115d86da19777fbf
Калоян Димитров <luckykaloyan@gmail.com>
2024-01-25 10:35:48 +0200

    Update

--- EntryModule/ConditionalStatements/ToyShop.js
+++ EntryModule/ConditionalStatements/ToyShop.js
@@ -6,29 +6,14 @@ function ToyShop(input){
     let teddies =  parseFloat(input[3])*4.10;
     let minions =  parseFloat(input[4])*8.20;
     let trucks =  parseFloat(input[5])*2;
-
-
     let number = parseFloat(input[1])+parseFloat(input[2])+parseFloat(input[3])+parseFloat(input[4])+parseFloat(input[5]);
     let total = puzzles+talkingdolls+teddies+minions+trucks;
     if(number>49){
-        total = total*0.75;
-    }
+        total = total*0.75;}
     total = total*0.90;
-    
-
     if(total>tripPrice){
         console.log("Yes "+(total-tripPrice).toFixed(2)+" lv left.")
     }else{
         console.log("Not enough money! "+(tripPrice-total).toFixed(2)+" lv needed.")
     }
-}ToyShop(["40.8",
-
-"20",
-
-"25",
-
-"30",
-
-"50",
-
-"10"])
+}
