92cdbeaee94cb227d4f209d64d31c580f4305efc
LuckyKaloyan <luckykaloyan@gmail.com>
2024-02-17 17:10:50 +0200

    Update

--- EntryModule/ForCycle/Excercises/Oscars.js
+++ EntryModule/ForCycle/Excercises/Oscars.js
@@ -19,26 +19,4 @@ if(academypoints<1250.5){
 }
 
 
-
-
-}Oscars(["Zahari Baharov",
-
-"205",
-
-"4",
-
-"Johnny Depp",
-
-"45",
-
-"Will Smith",
-
-"29",
-
-"Jet Lee",
-
-"10",
-
-"Matthew Mcconaughey",
-
-"39"])
+}
