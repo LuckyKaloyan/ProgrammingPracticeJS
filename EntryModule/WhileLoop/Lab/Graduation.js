194a3370fb0127f294ee599d85de36abae028c6d
Калоян Димитров <luckykaloyan@gmail.com>
2024-02-25 10:54:10 +0200

    Update

---
+++ EntryModule/WhileLoop/Lab/Graduation.js
@@ -0,0 +1,43 @@
+function Graduation(input){
+
+let index = 1;
+let timesFailed = 0;
+let total = 0;
+
+let name = String(input[0]);
+
+
+while(timesFailed<2){
+
+if(Number(input[index])<4.00){
+    timesFailed++;
+    total = total+Number(input[index]);
+}else{
+    total = total+Number(input[index]);
+}
+index++;
+if(index===13){break;}
+
+}
+
+if(timesFailed===2){
+    console.log(name+` has been excluded at `+index+` grade`);
+}else{
+    console.log(name+` graduated. Average grade: `+(total/12).toFixed(2));
+}
+
+
+
+}Graduation(["Gosho", 
+"5",
+"5.5",
+"6",
+"5.43",
+"5.5",
+"6",
+"5.55",
+"5",
+"6",
+"6",
+"5.43",
+"5"])
