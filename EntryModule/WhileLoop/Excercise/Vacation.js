a29ca44addbb11fbe5bab124316a7de3ce14c936
Калоян Димитров <luckykaloyan@gmail.com>
2024-02-27 11:07:10 +0200

    Update

--- EntryModule/WhileLoop/Excercise/Vacation.js
+++ EntryModule/WhileLoop/Excercise/Vacation.js
@@ -1,41 +1,32 @@
-function Vacation(input){
+function Vacation(input) {
+    let neededMoney = Number(input[0]);
+    let startingBudget = Number(input[1]);
+    let commandIndex = 2;
+    let amountIndex = 3;
+    let daysCounter = 0;
+    let consecutiveSpending = 0;
 
-let neededMoney = Number(input[0]);
-let startingBudget = Number(input[1]);
-let commandIndex = 2;
-let amountIndex = 3;
-let timesSpent = 0;
-let daysCounter = 0;
-let failed = false;
+    while (startingBudget < neededMoney && consecutiveSpending < 5) {
+        let command = input[commandIndex];
+        let amount = Number(input[amountIndex]);
 
-while(timesSpent<5){
-    daysCounter++;
-    command = input[commandIndex];
-if(command==="spend"){
-    startingBudget=startingBudget-input[amountIndex];
-    if(startingBudget<=0){
-      startingBudget=0;
-    }
-    timesSpent++;
-    if(timesSpent===5){
-        failed = true;
-        break;
-    }
-}else{
-    startingBudget=startingBudget+input[amountIndex];
-    timesSpent=0; 
-    if(startingBudget>=neededMoney){
-        break;
-    }
-}
-commandIndex=commandIndex+2;
-amountIndex=amountIndex+2;
+        if (command === "spend") {
+            startingBudget =startingBudget- amount;
+            consecutiveSpending++;
+        } else if (command === "save") {
+            startingBudget =startingBudget+ amount;
+            consecutiveSpending = 0;
+        }
 
-}
-if(failed){
-    console.log("You can't save the money.");
-    console.log(daysCounter);
-}else{
-    console.log("You saved the money for "+daysCounter+" days.");
-}
+        daysCounter++;
+        commandIndex = commandIndex+ 2;
+        amountIndex = amountIndex+ 2;
+    }
+
+    if (consecutiveSpending === 5) {
+        console.log("You can't save the money.");
+        console.log(daysCounter);
+    } else {
+        console.log(`You saved the money for ${daysCounter} days.`);
+    }
 }
