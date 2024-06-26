f2e0416d070c3056070297bbcb3c2c2370cd4b64
LuckyKaloyan <luckykaloyan@gmail.com>
2024-06-26 08:37:07 +0300

    Update

---
+++ FrontEnd/Syntax Fundamentals/Lab/largestnumber.js
@@ -0,0 +1,13 @@
+function largestnumber(input){
+
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+    let c = Number(input[2]);
+
+     a = Math.max(a,b);
+     c = Math.max(a,c);
+     console.log(c);
+
+
+
+}largestnumber([135, -31, 16])
--- FrontEnd/Syntax Fundamentals/Lab/mathoperations.js
+++ FrontEnd/Syntax Fundamentals/Lab/mathoperations.js
@@ -25,6 +25,4 @@ function mathoperations(input){
             console.log(a**b);
             break;
     }
-
-
-}mathoperations([4,5,'**'])
+}
