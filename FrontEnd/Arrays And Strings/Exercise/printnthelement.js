ca515cfa6799d6e7115e6eef1702e5c63323e67e
LuckyKaloyan <luckykaloyan@gmail.com>
2024-07-05 08:28:22 +0300

    Update

--- FrontEnd/Arrays And Strings/Exercise/printnthelement.js
+++ FrontEnd/Arrays And Strings/Exercise/printnthelement.js
@@ -1,17 +1,12 @@
 function solve(input, n) {
     let args = input;
     let nth = n;
-let newargs = [];
+    let newargs = [];
+
     for (let i = 0; i < args.length; i = i + nth) {
         newargs.push(args[i]);
     }
-  
-    console.log(newargs);
-}solve(['5', 
-'20', 
-'31', 
-'4', 
-'20'], 
-2)
+    return(newargs);
+}
 
 
