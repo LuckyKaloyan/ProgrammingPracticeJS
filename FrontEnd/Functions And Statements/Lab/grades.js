8b607199546a6573bc8ec5e44307d0c3c6cee610
LuckyKaloyan <luckykaloyan@gmail.com>
2024-07-08 08:13:43 +0300

    Update

--- FrontEnd/Functions And Statements/Lab/grades.js
+++ FrontEnd/Functions And Statements/Lab/grades.js
@@ -10,8 +10,8 @@ if(grade<3){
 }else if(grade>=3.5 && grade<4.5){
     console.log("Good ("+grade.toFixed(2)+")");
 }else if(grade>=4.50 && grade<5.5){
-    console.log("Very Good ("+grade.toFixed(2)+")");
+    console.log("Very good ("+grade.toFixed(2)+")");
 }else{ console.log("Excellent ("+grade.toFixed(2)+")");}
 
 
-}solve(4.50)
+}
