fa655dbb987e330ad61e875832b58098c9525206
Калоян Димитров <luckykaloyan@gmail.com>
2024-07-10 11:35:11 +0300

    Update

--- FrontEnd/Functions And Statements/Exercise/charactersinrange.js
+++ FrontEnd/Functions And Statements/Exercise/charactersinrange.js
@@ -1,7 +1,7 @@
 function solve(charA, charB) {
     let result = "";
     if (charA.charCodeAt(0) > charB.charCodeAt(0)) {
-        for (let i = charA.charCodeAt(0)-1; i > charB.charCodeAt(0); i--) {
+        for (let i = charB.charCodeAt(0)+1; i<charA.charCodeAt(0); i++) {
             result += String.fromCharCode(i) + " ";
         }
     }else{
@@ -10,6 +10,4 @@ function solve(charA, charB) {
         }
     }
     console.log(result);
-}solve('a',
-'d'
-)
+}
