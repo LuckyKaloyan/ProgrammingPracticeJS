4a6e0dfaed2159a35d2d71aa5dce84a5a460b5ca
Калоян Димитров <luckykaloyan@gmail.com>
2024-07-19 09:50:22 +0300

    Update

--- FrontEnd/Objects And Classes/Exercise/makeadictionary.js
+++ FrontEnd/Objects And Classes/Exercise/makeadictionary.js
@@ -6,8 +6,6 @@ let objects = [];
 for(let i=0; i<args.length; i++){
     let splitted = args[i].split(":");
 
-    
-    
     let object ={
               name:splitted[0],
               term:splitted[1],
@@ -18,7 +16,18 @@ for(let i=0; i<args.length; i++){
     object.name=nameSplit[1];
     object.term=termSplit[1];
 
-    objects.push(object);
+    let check = true;
+    for(let i=0; i<objects.length; i++){
+        if(object.name===objects[i].name){
+            check = false;
+            objects[i].name=nameSplit[1];
+            objects[i].term=termSplit[1];
+        }
+    }
+    if(check){
+        objects.push(object);
+    }
+    
 }
 
 objects.sort((a,b)=> {
@@ -34,13 +43,4 @@ objects.sort((a,b)=> {
 for(let i=0; i<objects.length; i++){
     console.log("Term: "+objects[i].name+" => Definition: "+objects[i].term)
 }
-
-
-}solve([
-    '{"Cup":"A small bowl-shaped container for drinking from, typically having a handle"}',
-    '{"Cake":"An item of soft sweet food made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated."} ',
-    '{"Watermelon":"The large fruit of a plant of the gourd family, with smooth green skin, red pulp, and watery juice."} ',
-    '{"Music":"Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."} ',
-    '{"Art":"The expression or application of human creative skill and imagination, typically in a visual form such as painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power."} '
-    ]
-    )
+}
