5d0be430726af52de49a9f3add197c51a4e4413d
Калоян Димитров <luckykaloyan@gmail.com>
2024-07-20 08:53:03 +0300

    Update

--- FrontEnd/DOM Introduction/Lab/02/CollectListItems.js
+++ FrontEnd/DOM Introduction/Lab/02/CollectListItems.js
@@ -5,7 +5,12 @@ let liElements = document.getElementById('items').getElementsByTagName('li');
 let word = "";
 
 for(let i=0; i<liElements.length; i++){
-    word=word+liElements[i].innerText+"\n";
+    if(i==liElements.length-1){
+        word=word+liElements[i].innerText
+    }else{
+        word=word+liElements[i].innerText+"\n";
+    }
+    
 }
 
    document.getElementById('result').value=word;
