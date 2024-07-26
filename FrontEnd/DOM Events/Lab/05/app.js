0173ece5d649f30ecd429bae5e9dc5a72d302246
LuckyKaloyan <luckykaloyan@gmail.com>
2024-07-26 23:52:45 +0300

    Update

--- FrontEnd/DOM Events/Lab/05/app.js
+++ FrontEnd/DOM Events/Lab/05/app.js
@@ -1,3 +1,23 @@
 function focused() {
-    console.log('TODO:...');
+    let inputs = document.querySelectorAll('input');
+
+    inputs.forEach(function(input) {
+        input.addEventListener('focus', function(e) {
+            document.querySelectorAll('div').forEach(function(div) {
+                div.classList.remove('focused');
+            });
+
+            let parentDiv = e.target.closest('div');
+            if (parentDiv) {
+                parentDiv.classList.add('focused');
+            }
+        });
+
+        input.addEventListener('blur', function(e) {
+            let parentDiv = e.target.closest('div');
+            if (parentDiv) {
+                parentDiv.classList.remove('focused');
+            }
+        });
+    });
 }
