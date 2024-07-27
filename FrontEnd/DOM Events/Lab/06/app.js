fc12a89d9614740feb6df79d7f2e660c06b54736
LuckyKaloyan <luckykaloyan@gmail.com>
2024-07-27 10:55:07 +0300

    Update

--- FrontEnd/DOM Events/Lab/06/app.js
+++ FrontEnd/DOM Events/Lab/06/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    function validateEmail() {
+        let input = document.getElementById('email').value;
+        let regex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if (!regex.test(input)) {
+            document.getElementById('email').classList.add('error');
+        } else {
+            document.getElementById('email').classList.remove('error');
+        }
+    }
+    document.getElementById('email').addEventListener('change', validateEmail);
 }
