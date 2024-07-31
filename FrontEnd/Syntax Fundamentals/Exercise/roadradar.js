8d7abf0f53d5f314dbb43fb2123fbcadf7359ef1
LuckyKaloyan <luckykaloyan@gmail.com>
2024-07-31 09:45:58 +0300

    Update

--- FrontEnd/Syntax Fundamentals/Exercise/roadradar.js
+++ FrontEnd/Syntax Fundamentals/Exercise/roadradar.js
@@ -1,76 +1,38 @@
-function solve(numericValue, stringValue){
-
-let speed = Number(numericValue);
-let limitType = String(stringValue);
-let status;
-switch(limitType){
-    case "city" :
-        
-        if(speed>50){
-            
-            if(speed-50 <=20){
-                status = "speeding";
-            }else if(speed-50<=40){
-                status = "excessive speeding";
-            }else{
-                status = "reckless driving";
-            }
-            console.log('The speed is '+(speed-50)+'km/h faster than the allowed speed of '+50+" - "+status);
-            }else{
-                console.log("Driving "+speed+" km/h in a 50 zone")
-            }
-
-            
+function solve(numericValue, stringValue) {
+    let speed = Number(numericValue);
+    let limitType = String(stringValue).toLowerCase();
+    let status;
+    let limit;
+    
+    switch (limitType) {
+        case "city":
+            limit = 50;
             break;
-    case "residential":
-        
-        if(speed>20){
-            
-            if(speed-20 <=20){
-                status = "speeding";
-            }else if(speed-20<=40){
-                status = "excessive speeding";
-            }else{
-                status = "reckless driving";
-            }
-            console.log('The speed is '+(speed-20)+'km/h faster than the allowed speed of '+20+" - "+status);
-            }else{
-                console.log("Driving "+speed+" km/h in a 20 zone")
-            }
-
-           
+        case "residential":
+            limit = 20;
             break;
-       case "interstate":
-        
-        if(speed>90){
-            if(speed-90 <=20){
-                status = "speeding";
-            }else if(speed-90<=40){
-                status = "excessive speeding";
-            }else{
-                status = "reckless driving";
-            }
-            console.log('The speed is '+(speed-90)+'km/h faster than the allowed speed of '+90+" - "+status);
-            }else{
-                console.log("Driving "+speed+" km/h in a 90 zone")
-            }
+        case "interstate":
+            limit = 90;
             break;
-
-       case "motorway":
-        
-        if(speed>130){
-            
-            if(speed-130 <=20){
-                status = "speeding";
-            }else if(speed-130<=40){
-                status = "excessive speeding";
-            }else{
-                status = "reckless driving";
-            }
-            console.log('The speed is '+(speed-130)+'km/h faster than the allowed speed of '+130+" - "+status);
-            }else{
-                console.log("Driving "+speed+" km/h in a 130 zone")
-            }
+        case "motorway":
+            limit = 130;
             break;
-}
+        default:
+            console.log("Invalid area type");
+            return; 
+    }
+    
+    if (speed > limit) {
+        let difference = speed - limit;
+        if (difference <= 20) {
+            status = "speeding";
+        } else if (difference <= 40) {
+            status = "excessive speeding";
+        } else {
+            status = "reckless driving";
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of ${limit} - ${status}`);
+    } else {
+        console.log(`Driving ${speed} km/h in a ${limit} zone`);
+    }
 }
